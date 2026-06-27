1ae5707120cd4ec533100e19d293dfb7e76f37e2
GXLCLC <2193160597@qq.com>
2026-06-27 11:13:14 +0800

    Update feeds.sh
    Signed-off-by: GXLCLC <2193160597@qq.com>

--- wrt_core/modules/feeds.sh
+++ wrt_core/modules/feeds.sh
@@ -34,6 +34,10 @@ update_feeds() {
     #添加新的DDNSTO源
     append_feed_if_missing "$FEEDS_PATH" "nas" "src-git nas https://github.com/linkease/nas-packages.git;master"
     append_feed_if_missing "$FEEDS_PATH" "nas_luci" "src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main"
+    #添加turboacc源
+    append_feed_if_missing "turboacc" "src-git turboacc https://gitcode.com/gh_mirrors/tu/turboacc.git;main"
+
+    
     if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
         touch "$BUILD_DIR/include/bpf.mk"
     fi
