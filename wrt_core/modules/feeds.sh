e989de240b8669f62c61c9d768b57291ad202421
GXLCLC <2193160597@qq.com>
2026-06-27 12:21:38 +0800

    Update feeds.sh
    Signed-off-by: GXLCLC <2193160597@qq.com>

--- wrt_core/modules/feeds.sh
+++ wrt_core/modules/feeds.sh
@@ -35,7 +35,7 @@ update_feeds() {
     append_feed_if_missing "$FEEDS_PATH" "nas" "src-git nas https://github.com/linkease/nas-packages.git;master"
     append_feed_if_missing "$FEEDS_PATH" "nas_luci" "src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main"
     #添加turboacc源
-    append_feed_if_missing "$FEEDS_PATH" "luci-app-turboacc" "src-git luci-app-turboacc https://github.com/chenmozhijin/turboacc.git;luci"
+    append_feed_if_missing "$FEEDS_PATH" "luci-app-turboacc" "src-git luci-app-turboacc https://github.com/chenmozhijin/turboacc.git;package"
 
     
     if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
