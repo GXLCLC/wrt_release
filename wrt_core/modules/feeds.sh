ddcb3a8b3ae95505b5de61381beb6ff2db53f5c4
GXLCLC <2193160597@qq.com>
2026-06-27 13:22:03 +0800

    Update feeds.sh
    Signed-off-by: GXLCLC <2193160597@qq.com>

--- wrt_core/modules/feeds.sh
+++ wrt_core/modules/feeds.sh
@@ -14,6 +14,7 @@ append_feed_if_missing() {
     local feed_entry="$3"
 
     if ! grep -q "$match_pattern" "$feeds_path"; then
+        [ -z "$(tail -c 1 "$feeds_path")" ] || echo "" >>"$feeds_path"
         echo "$feed_entry" >>"$feeds_path"
     fi
 }
@@ -21,21 +22,16 @@ append_feed_if_missing() {
 update_feeds() {
     local FEEDS_PATH
     FEEDS_PATH=$(get_feeds_path)
+    sed -i '/^#/d' "$FEEDS_PATH"
     sed -i '/packages_ext/d' "$FEEDS_PATH"
     sed -i '/[[:space:]]small8[[:space:]]/d' "$FEEDS_PATH"
     sed -i '/[[:space:]]custom_feed[[:space:]]/d' "$FEEDS_PATH"
 
     append_feed_if_missing "$FEEDS_PATH" "openwrt_bandix" "src-git openwrt_bandix https://github.com/timsaya/openwrt-bandix.git;main"
     append_feed_if_missing "$FEEDS_PATH" "luci_app_bandix" "src-git luci_app_bandix https://github.com/timsaya/luci-app-bandix.git;main"
-    #过时原仓库迁移
-    #append_feed_if_missing "$FEEDS_PATH" "ddnsto" "src-git ddnsto https://github.com/linkease/ddnsto-openwrt.git;main"
-    #添加新的DDNSTO源
+    #不要了过时append_feed_if_missing "$FEEDS_PATH" "ddnsto" "src-git ddnsto https://github.com/linkease/ddnsto-openwrt.git;main"
     append_feed_if_missing "$FEEDS_PATH" "nas" "src-git nas https://github.com/linkease/nas-packages.git;master"
     append_feed_if_missing "$FEEDS_PATH" "nas_luci" "src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main"
-    #添加turboacc源
-    append_feed_if_missing "$FEEDS_PATH" "turboacc-luci" "src-git turboacc-luci https://github.com/chenmozhijin/turboacc.git;luci"
-    # TurboACC 底层SFE/FullCone内核模块（package分支）
-    append_feed_if_missing "$FEEDS_PATH" "turboacc-core" "src-git turboacc-core https://github.com/chenmozhijin/turboacc.git;package"
     if [ ! -f "$BUILD_DIR/include/bpf.mk" ]; then
         touch "$BUILD_DIR/include/bpf.mk"
     fi
