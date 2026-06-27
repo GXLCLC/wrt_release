13e21b6152ed4bb1f145a1c90bcb8c97793970ec
GXLCLC <2193160597@qq.com>
2026-06-27 11:02:35 +0800

    Update packages.sh
    Signed-off-by: GXLCLC <2193160597@qq.com>

--- wrt_core/modules/packages.sh
+++ wrt_core/modules/packages.sh
@@ -670,10 +670,3 @@ update_package() {
         echo "更新软件包 $1 到 $PKG_VER $PKG_HASH"
     fi
 }
-# TurboACC 网络加速插件
-sync_sparse_packages_to_feed_dir \
-  "https://github.com/chenmozhijin/turboacc.git" \
-  "luci" \
-  "$BUILD_DIR/package/feeds/custom_feed" \
-  "turboacc"
-
